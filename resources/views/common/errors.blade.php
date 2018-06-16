f15979bdbe445a0e87ab8a3caeba56528c2522c0
root <jim112828@hotmail.com>
2018-06-16 07:15:03 +0000

    first commit

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,18 @@
+@if (count($errors)>0)
+	
+	<div class="alert aler-danger">
+		<strong>Whoops! Something went wrong!</strong>
+
+		<br><br>
+
+		<ul>
+			@foreach($errors -> all() as $error)
+				<li>{{$error}}</li>
+			@endforeach
+
+
+		</ul>
+
+
+	</div>
+@endif
